b5ea59009be92a4ca1ca262257b03e6f40f91da9
saifali-pal <saif.ali@mergestack.com>
2025-11-05 16:45:45 +0500

    multiple tasks and random

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Card className="w-full max-w-md p-4">
         <CardContent>
           <h1 className="text-2xl font-bold mb-4 text-center">
-            Multiple Tasks
+            Multiple Tasks and random
           </h1>
           <TodoFilters />
           <TodoList />
