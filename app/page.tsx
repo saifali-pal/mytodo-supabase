676cdbd345cff10dd363a844391f91e8eba75d15
saifali-pal <saif.ali@mergestack.com>
2025-11-05 15:55:52 +0500

    feat: filters-and-search

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import TodoList from "@/components/todo/TodoList";
 import { Card, CardContent } from "@/components/ui/card";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import TodoFilters from "@/components/todo/TodoFilters";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Card className="w-full max-w-md p-4">
         <CardContent>
           <h1 className="text-2xl font-bold mb-4 text-center">All Tasks</h1>
+          <TodoFilters />
           <TodoList />
         </CardContent>
       </Card>
